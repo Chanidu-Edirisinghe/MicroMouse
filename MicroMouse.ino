efbcda373a84966ad89655f2d0177bb0a64cd733
Chanidu-Edirisinghe <chanidu1214@gmail.com>
2024-08-23 09:04:53 +0530

    Commit 6
    Working with old motors, need to add pid controls.

--- MicroMouse.ino
+++ MicroMouse.ino
@@ -1,25 +1,29 @@
-const int frontSensorPin = A0;
-const int leftSensorPin = A1;
-const int rightSensorPin = A2;
+const int sensorPin[] = {A0, A1, A2};
+float distance[3];
+const int AVERAGE_OF = 200;
+const float MCU_VOLTAGE = 5.0;
 
-const int leftMotorPin = 9; 
-const int rightMotorPin = 10;
+int enA = 9;
+int in1 = 4;
+int in2 = 5;
 
-float Kp = 0;
-float Kd = 0;
+int enB = 6;
+int in3 = 7;
+int in4 = 8;
 
-float previousErrorLeft = 0;
-float previousErrorRight = 0;
-float setPoint = 4.6;
+float kp = 1.0;  // Proportional gain
+float kd = 0.0;  // Derivative gain
+float ki = 0.0; // Integral gain
 
-unsigned long previousTime = 0;
-unsigned long currentTime;
+float ePrevious = 0;
+float eIntegral = 0;
+long previousTime = 0;
 
-const int MAZE_SIZE = 16;
+const int MAZE_SIZE = 8;
 int maze[MAZE_SIZE][MAZE_SIZE] = {0};  // 2D array to represent walls
 int distances[MAZE_SIZE][MAZE_SIZE];   // 2D array to store distances
 
-int currentX = 15;
+int currentX = 7;
 int currentY = 0;
 int currentOrientation = 4;
 
@@ -27,8 +31,10 @@ struct Cell {
   int x, y;
 };
 
+Cell start = { currentX, currentY };
+
 const int targetCells[4][2] = {
-    {7, 7}, {7, 8}, {8, 7}, {8, 8}
+    {3, 3}, {3, 4}, {4, 3}, {4, 4}
 };
 
 struct Node {
@@ -97,30 +103,72 @@ public:
 };
 
 void turnRight() {
-    // Your robot's turn right logic
+  analogWrite(enA, 255);
+  analogWrite(enB, 255);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in1, LOW);
+  digitalWrite(in4, HIGH);
+  digitalWrite(in3, LOW);
+  delay(655);
+  stop();
 }
 
 void turnLeft() {
-    // Your robot's turn left logic
+  analogWrite(enA, 255);
+  analogWrite(enB, 255);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  delay(655);
+  stop();
 }
 
 void moveForward() {
-    // Your robot's move forward logic
+  analogWrite(enB, 255);
+  analogWrite(enA, 255);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in4, HIGH);
+  digitalWrite(in3, LOW);
+  delay(1400);
+  stop();
+}
+
+void stop() {
+  analogWrite(enA, 0);
+  analogWrite(enB, 0);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
 }
 
 bool wallFront(){
-  int frontDistance = analogRead(frontSensorPin);
-  return true;
+  if (distance[2] > 6) {
+    return false;
+  }
+  else{
+    return true;
+  }
 }
 
 bool wallRight(){
-  int rightDistance = analogRead(rightSensorPin);
-  return true;
+  if (distance[0] > 20) {
+    return false;
+  }
+  else{
+    return true;
+  }
 }
 
 bool wallLeft(){
-  int leftDistance = analogRead(leftSensorPin);
-  return true;
+  if (distance[1] > 20) {
+    return false;
+  }
+  else{
+    return true;
+  }
 }
 
 void floodFill() {
@@ -141,10 +189,10 @@ void floodFill() {
     };
 
     for (int i = 0; i < 4; i++) {
-    int tx = targetCells[i][0];
-    int ty = targetCells[i][1];
-    distances[tx][ty] = 0;  // Distance to target cells is 0
-    queue.enqueue({tx, ty});
+      int tx = targetCells[i][0];
+      int ty = targetCells[i][1];
+      distances[tx][ty] = 0;  // Distance to target cells is 0
+      queue.enqueue({tx, ty});
     }
   }
   else{
@@ -177,49 +225,71 @@ void floodFill() {
   }
 }
 
-void PID(){
-  // convert the values to distances
-  int frontDistance = analogRead(frontSensorPin);
-  int leftDistance = analogRead(leftSensorPin);
-  int rightDistance = analogRead(rightSensorPin);
+// void PID(){
+//   // convert the values to distances
+//   int frontDistance = analogRead(frontSensorPin);
+//   int leftDistance = analogRead(leftSensorPin);
+//   int rightDistance = analogRead(rightSensorPin);
 
-  currentTime = millis();
-  float elapsedTime = (currentTime - previousTime) / 1000.0;
+//   currentTime = millis();
+//   float elapsedTime = (currentTime - previousTime) / 1000.0;
   
-  float errorLeft = setPoint - leftDistance;
-  float errorRight = setPoint - rightDistance;
+//   float errorLeft = setPoint - leftDistance;
+//   float errorRight = setPoint - rightDistance;
 
-  float PoutLeft = Kp * errorLeft;
-  float PoutRight = Kp * errorRight;
+//   float PoutLeft = Kp * errorLeft;
+//   float PoutRight = Kp * errorRight;
 
-  float derivativeLeft = (errorLeft - previousErrorLeft) / elapsedTime;
-  float derivativeRight = (errorRight - previousErrorRight) / elapsedTime;
-  float DoutLeft = Kd * derivativeLeft;
-  float DoutRight = Kd * derivativeRight;
+//   float derivativeLeft = (errorLeft - previousErrorLeft) / elapsedTime;
+//   float derivativeRight = (errorRight - previousErrorRight) / elapsedTime;
+//   float DoutLeft = Kd * derivativeLeft;
+//   float DoutRight = Kd * derivativeRight;
 
-  float PIDoutLeft = PoutLeft + DoutLeft;
-  float PIDoutRight = PoutRight + DoutRight;
+//   float PIDoutLeft = PoutLeft + DoutLeft;
+//   float PIDoutRight = PoutRight + DoutRight;
 
-  int baseSpeed = 100;  
-  int leftMotorSpeed = baseSpeed + PIDoutLeft;
-  int rightMotorSpeed = baseSpeed + PIDoutRight;
+//   int baseSpeed = 100;  
+//   int leftMotorSpeed = baseSpeed + PIDoutLeft;
+//   int rightMotorSpeed = baseSpeed + PIDoutRight;
 
-  leftMotorSpeed = constrain(leftMotorSpeed, 0, 255);
-  rightMotorSpeed = constrain(rightMotorSpeed, 0, 255);
+//   leftMotorSpeed = constrain(leftMotorSpeed, 0, 255);
+//   rightMotorSpeed = constrain(rightMotorSpeed, 0, 255);
 
-  analogWrite(leftMotorPin, leftMotorSpeed);
-  analogWrite(rightMotorPin, rightMotorSpeed);
+//   analogWrite(leftMotorPin, leftMotorSpeed);
+//   analogWrite(rightMotorPin, rightMotorSpeed);
 
-  previousErrorLeft = errorLeft;
-  previousErrorRight = errorRight;
-  previousTime = currentTime;
+//   previousErrorLeft = errorLeft;
+//   previousErrorRight = errorRight;
+//   previousTime = currentTime;
 
-  if (frontDistance < setPoint) {  
-    analogWrite(leftMotorPin, 0);
-    analogWrite(rightMotorPin, 0);
+//   if (frontDistance < setPoint) {  
+//     analogWrite(leftMotorPin, 0);
+//     analogWrite(rightMotorPin, 0);
+//   }
+
+//   delay(50);
+// }
+
+void readFrontDistance(int sensor) {
+  float voltage_temp_average = 0;
+  for (int i = 0; i < AVERAGE_OF; i++) {
+    int sensorValue = analogRead(sensorPin[sensor]);
+    voltage_temp_average += sensorValue * (MCU_VOLTAGE / 1023.0);
   }
+  voltage_temp_average /= AVERAGE_OF;
 
-  delay(50);
+  distance[sensor] = 13 * pow(voltage_temp_average, -1);
+}
+
+void readDistance(int sensor) {
+  float voltage_temp_average = 0;
+  for (int i = 0; i < AVERAGE_OF; i++) {
+    int sensorValue = analogRead(sensorPin[sensor]);
+    voltage_temp_average += sensorValue * (MCU_VOLTAGE / 1023.0);
+  }
+  voltage_temp_average /= AVERAGE_OF;
+
+  distance[sensor] = 34.6 + -69.5 * (voltage_temp_average) + 62.3 * pow(voltage_temp_average, 2) + -25.4 * pow(voltage_temp_average, 3) + 3.83 * pow(voltage_temp_average, 4);
 }
 
 void updateMaze(bool left, bool right, bool front, bool back, Cell currentPosition) {
@@ -315,50 +385,56 @@ void navigate(Cell currentPosition, int orientation) {
         {-1, 0},  // Forward (4)
         {1, 0}    // Backward (8)
     };
-
+    
     while (true) {
+      readDistance(0);
+      readDistance(1);
+      readFrontDistance(2);
+  
         // Check surroundings
-        bool right = wallRight();
-        bool left = wallLeft();
-        bool front = wallFront();
-        bool back = false;  // The robot cannot directly sense back
+      bool right = wallRight();
+      bool left = wallLeft();
+      bool front = wallFront();
+      bool back = false;  // The robot cannot directly sense back
+      Serial.println(right);
+      Serial.println(left);
+      Serial.println(front);
+      // Adjust walls according to the current orientation
+      bool adjustedFront, adjustedRight, adjustedLeft, adjustedBack;
 
-        // Adjust walls according to the current orientation
-        bool adjustedFront, adjustedRight, adjustedLeft, adjustedBack;
-
-        switch (orientation) {
-            case 4: // Facing forward
-                adjustedFront = front;
-                adjustedRight = right;
-                adjustedLeft = left;
-                adjustedBack = back;
-                break;
-            case 1: // Facing right
-                adjustedFront = left;
-                adjustedRight = front;
-                adjustedLeft = back;
-                adjustedBack = right;
-                break;
-            case 8: // Facing backward
-                adjustedFront = back;
-                adjustedRight = left;
-                adjustedLeft = right;
-                adjustedBack = front;
-                break;
-            case 2: // Facing left
-                adjustedFront = right;
-                adjustedRight = back;
-                adjustedLeft = front;
-                adjustedBack = left;
-                break;
-            default:
-                Serial.println("Unexpected orientation!");
-                return;
+      switch (orientation) {
+          case 4: // Facing forward
+              adjustedFront = front;
+              adjustedRight = right;
+              adjustedLeft = left;
+              adjustedBack = back;
+              break;
+          case 1: // Facing right
+              adjustedFront = left;
+              adjustedRight = front;
+              adjustedLeft = back;
+              adjustedBack = right;
+              break;
+          case 8: // Facing backward
+              adjustedFront = back;
+              adjustedRight = left;
+              adjustedLeft = right;
+              adjustedBack = front;
+              break;
+          case 2: // Facing left
+              adjustedFront = right;
+              adjustedRight = back;
+              adjustedLeft = front;
+              adjustedBack = left;
+              break;
+          default:
+              Serial.println("Unexpected orientation!");
+              return;
         }
 
         // Update the maze with adjusted walls
         updateMaze(adjustedLeft, adjustedRight, adjustedFront, adjustedBack, currentPosition);
-
+        Serial.println("Maze updated");
         // Recalculate distances with flood fill
         floodFill();
         // for (int i = 0; i < MAZE_SIZE; i++) {
@@ -380,19 +456,24 @@ void navigate(Cell currentPosition, int orientation) {
         int minDistance = 1000;
         Cell nextPosition = { -1, -1 };
         int nextOrientation = orientation;
-
+        // Serial.println(currentPosition.x);
+        // Serial.println(currentPosition.y);
         for (int i = 0; i < 4; i++) {
             int newX = currentPosition.x + directions[i][0];
             int newY = currentPosition.y + directions[i][1];
-
+            Serial.println(newX);
+            Serial.println(newY);
             if (newX >= 0 && newX < MAZE_SIZE && newY >= 0 && newY < MAZE_SIZE) {
                 if (distances[newX][newY] < minDistance && (maze[currentPosition.x][currentPosition.y] & (1 << i)) == 0) {
                     minDistance = distances[newX][newY];
+                    Serial.println("next Position updated");
                     nextPosition = { newX, newY };
                     nextOrientation = 1 << i;  // Update the orientation based on the movement direction
                 }
             }
         }
+        Serial.println(nextPosition.x);
+        Serial.println(nextPosition.y);
 
         // If nextPosition is invalid, it means the robot is stuck
         if (nextPosition.x == -1 && nextPosition.y == -1) {
@@ -421,10 +502,23 @@ void navigate(Cell currentPosition, int orientation) {
 void setup() {
   Serial.begin(9600);
 
-  pinMode(leftMotorPin, OUTPUT);
-  pinMode(rightMotorPin, OUTPUT);
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
 
   floodFill();
+  for (int i = 0; i < MAZE_SIZE; i++) {
+    for (int j = 0; j < MAZE_SIZE; j++) {
+        Serial.print(distances[i][j]);
+        Serial.print(" ");
+    }
+    Serial.println("");
+  }
+
+  navigate(start, currentOrientation);
   // for (int row = 0; row < MAZE_SIZE; row++) {
   //   for (int col = 0; col < MAZE_SIZE; col++) {
   //     Serial.print(distances[row][col]);
@@ -441,7 +535,7 @@ void setup() {
 }
 
 void loop() {
-  PID();
+  
   
 }
 
